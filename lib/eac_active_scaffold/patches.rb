c27fd6963a3945130a81dec2ea5fda1e67a63094
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-09-04 00:40:36 -0300

    EacActiveScaffold::Patches.

---
+++ lib/eac_active_scaffold/patches.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+
+module EacActiveScaffold
+  module Patches
+    require_sub __FILE__, base: nil, require_dependency: false
+  end
+end
