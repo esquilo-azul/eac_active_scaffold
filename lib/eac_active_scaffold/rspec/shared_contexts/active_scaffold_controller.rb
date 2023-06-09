2a2238973d829c76777a3891b4f5f4c7e291acdb
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-06-09 16:57:50 -0300

    RSpec.shared_context('active_scaffold_controller'): refact.

--- lib/eac_active_scaffold/rspec/shared_contexts/active_scaffold_controller.rb
+++ lib/eac_active_scaffold/rspec/shared_contexts/active_scaffold_controller.rb
@@ -13,13 +13,20 @@ require 'eac_active_scaffold/rspec/controller_director'
     expect(page).to have_content director.page_title
   end
 
-  it 'create new record' do
-    expect do
+  context 'when record is created' do
+    let(:before_create_count) { director.model_class.count }
+
+    before do
+      before_create_count
       click_on ::I18n.t('active_scaffold.create_new')
       director.valid_create_data.each do |key, value|
         fill_in director.attribute_label(key), with: value
       end
       click_on ::I18n.t('active_scaffold.create')
-    end.to change { director.model_class.count }.by(1)
+    end
+
+    it 'increments record count' do
+      expect(director.model_class.count).to eq(before_create_count + 1)
+    end
   end
 end
