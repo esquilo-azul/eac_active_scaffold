360d030dac21258fb40e94f5f3a28d78a7c0f3d9
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-09-09 20:20:22 -0300

    EacActiveScaffold: remove setup with EacRubyUtils::RootModuleSetup.

--- lib/eac_active_scaffold.rb
+++ lib/eac_active_scaffold.rb
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
 
-require 'eac_ruby_utils'
-EacRubyUtils::RootModuleSetup.perform __FILE__
-
 module EacActiveScaffold
 end
-
-require 'eac_rails_utils'
-require 'active_scaffold'
-require 'recordselect'
 
 require 'eac_active_scaffold/engine'
--- lib/eac_active_scaffold/engine.rb
+++ lib/eac_active_scaffold/engine.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
+require 'eac_ruby_utils'
+require 'eac_rails_utils'
 require 'active_scaffold'
-require 'eac_active_scaffold/patches'
-require 'eac_rails_utils/engine_helper'
 require 'recordselect'
+require 'eac_active_scaffold/patches'
 
 module EacActiveScaffold
   class Engine < ::Rails::Engine
