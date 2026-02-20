29a82f38147629b4ae06865fbf2bc849075ff05e
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-02-20 14:05:16 -0300

    EacActiveScaffold: setup with Rails::Engine.

--- lib/eac_active_scaffold/engine.rb
+++ lib/eac_active_scaffold/engine.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
+require 'eac_rails_utils'
+require 'eac_ruby_utils'
+require 'active_scaffold'
+require 'recordselect'
+
 module EacActiveScaffold
   class Engine < ::Rails::Engine
     include ::EacRailsUtils::EngineHelper
   end
 end
+
+require 'eac_active_scaffold/patches'
